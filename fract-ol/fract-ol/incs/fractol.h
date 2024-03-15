8d46495edef104ccb7fdc85df32797aebc882d19
AIS <arttuilamarisalo@gmail.com>
2024-03-15 13:56:01 +0200

    fractol working version

---
+++ fract-ol/fract-ol/incs/fractol.h
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalo <asalo@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 10:20:41 by asalo             #+#    #+#             */
+/*   Updated: 2024/03/15 12:52:35 by asalo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "../../mlx/mlx.h"
+# include "../../libft/incs/libft.h"
+# include <math.h>
+
+# define WIDTH 800
+# define HIGHT 800
+
+/* KEYCODES */
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define UP 126
+# define DOWN 125
+# define LEFT 123
+# define RIGHT 124
+# define R 15
+# define C 8
+# define J 38
+# define P 35
+# define M 46
+# define ESC 53
+
+/* MOUSECODES */
+# define SCROLL_UP 5
+# define SCROLL_DOWN 4
+
+typedef struct s_fractal
+{
+	void	*mlx;
+	void	*window;
+	void	*image;
+	void	*pointer_to_image;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+	int		x;
+	int		y;
+	double	zx;
+	double	zy;
+	double	cx;
+	double	cy;
+	int		color;
+	double	offset_x;
+	double	offset_y;
+	double	zoom;
+	char	*name;
+	int		max_iterations;
+}			t_fractal;
+
+void		check_input(char *option);
+int			clean_exit(t_fractal *fractal);
+void		change_iterations(t_fractal *fractal, int key_code);
+void		init_fractal(t_fractal *fractal);
+void		init_mlx(t_fractal *fractal);
+void		calculate_mandelbrot(t_fractal *fractal);
+void		calculate_julia(t_fractal *fractal);
+int			draw_fractal(t_fractal *fractal, char *option);
+void		color_pixel(t_fractal *fractal, int x, int y, int colour);
+void		*draw_mandelbrot(void *fractal_void);
+void		draw_julia(t_fractal *fractal);
+int			key_hook(int key_code, t_fractal *fractal);
+int			mouse_hook(int mouse_code, int x, int y, t_fractal *fractal);
+void		*draw_mandelbrot(void *fractal_void);
+void		draw_julia(t_fractal *fractal);
+
+#endif
---
+++ fract-ol/fract-ol/src/mandel.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mandel.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalo <asalo@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 09:11:58 by asalo             #+#    #+#             */
+/*   Updated: 2024/03/15 10:46:45 by asalo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/fractol.h"
+
+void	calculate_mandelbrot(t_fractal *fractal)
+{
+	int		i;
+	double	tmp;
+
+	fractal->name = "mandel";
+	i = 0;
+	fractal->zx = 0.0;
+	fractal->zy = 0.0;
+	fractal->cx = (fractal->x / fractal->zoom) + fractal->offset_x;
+	fractal->cy = (fractal->y / fractal->zoom) + fractal->offset_y;
+	while (++i < fractal->max_iterations)
+	{
+		tmp = fractal->zx * fractal->zx - fractal->zy * fractal->zy
+			+ fractal->cx;
+		fractal->zy = 2. * fractal->zx * fractal->zy + fractal->cy;
+		fractal->zx = tmp;
+		if (fractal->zx * fractal->zx + fractal->zy
+			* fractal->zy >= __DBL_MAX__)
+			break ;
+	}
+	if (i == fractal->max_iterations)
+		color_pixel(fractal, fractal->x, fractal->y, 0xFFFFFF);
+	else
+		color_pixel(fractal, fractal->x, fractal->y, (fractal->color
+				* i));
+}
+
+void	*draw_mandelbrot(void *fractal_void)
+{
+	t_fractal	*fractal;
+
+	fractal = (t_fractal *)fractal_void;
+	fractal->x = 0;
+	fractal->y = 0;
+	while (fractal->x < WIDTH)
+	{
+		while (fractal->y < HIGHT)
+		{
+			calculate_mandelbrot(fractal);
+			fractal->y++;
+		}
+		fractal->x++;
+		fractal->y = 0;
+	}
+	return (NULL);
+}
